3fe4f7aae73ccde0bc3a0a2cb52b0263f5e1edc7
benjamin <carlosbpac@gmail.com>
2022-02-14 22:41:06 -0600

    Estrucure of proyect, achives and folders

---
+++ Source-JFLEX&CUP/temp-code/demo/src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!!!");
+    }
+}
