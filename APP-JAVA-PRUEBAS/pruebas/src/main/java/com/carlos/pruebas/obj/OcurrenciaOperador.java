2d7425a69d95dd50480a80b8a318d9a905184b9e
benjamin <carlosbpac@gmail.com>
2022-02-20 21:03:47 -0600

    objeto ocurrencia de un operador matematico

---
+++ APP-JAVA-PRUEBAS/pruebas/src/main/java/com/carlos/pruebas/obj/OcurrenciaOperador.java
@@ -0,0 +1,54 @@
+package com.carlos.pruebas.obj;
+
+public class OcurrenciaOperador {
+    private String operador;
+    private int linea;
+    private int columna;
+    private String ocurrencia;
+    public OcurrenciaOperador(){
+
+    }
+    public OcurrenciaOperador(String operador, int linea, int columna, String ocurrencia) {
+        this.operador = operador;
+        this.linea = linea;
+        this.columna = columna;
+        this.ocurrencia = ocurrencia;
+    }
+
+    public String getOperador() {
+        return operador;
+    }
+
+    public void setOperador(String operador) {
+        this.operador = operador;
+    }
+
+    public int getLinea() {
+        return linea;
+    }
+
+    public void setLinea(int linea) {
+        this.linea = linea;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+
+    public String getOcurrencia() {
+        return ocurrencia;
+    }
+
+    public void setOcurrencia(String ocurrencia) {
+        this.ocurrencia = ocurrencia;
+    }
+
+    @Override
+    public String toString() {
+        return "OcurrenciaOperador{" + "operador=" + operador + ", linea=" + linea + ", columna=" + columna + ", ocurrencia=" + ocurrencia + '}';
+    }
+}
