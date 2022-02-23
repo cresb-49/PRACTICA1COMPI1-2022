7be80f38c39f333051d9b79edf2f2a376793a79c
benjamin <carlosbpac@gmail.com>
2022-02-23 02:46:43 -0600

    Actualizacion del parser en android app

--- Android-App-Source/app/src/main/java/com/carlos/android_practica1/backened/obj/VerficacionValuesPie.java
+++ Android-App-Source/app/src/main/java/com/carlos/android_practica1/backened/obj/VerficacionValuesPie.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.carlos.pruebas.obj;
+package com.carlos.android_practica1.backened.obj;
 
 import java.util.Arrays;
 
