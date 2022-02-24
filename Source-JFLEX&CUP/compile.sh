e8b3ebde239828872d503e971acba0ddc3649e2d
benjamin <carlosbpac@gmail.com>
2022-02-23 22:45:21 -0600

    organizacion de codigo

--- Source-JFLEX&CUP/compile.sh
+++ Source-JFLEX&CUP/compile.sh
@@ -6,9 +6,9 @@ echo "STARTING JAVA CUP COMPILING"
 cup parser.cup
 
 echo "COPY FILES"
-cp Lexer.java ../APP-JAVA-PRUEBAS/pruebas/src/main/java/com/carlos/pruebas/lexerParser
-cp ParserGraphics.java ../APP-JAVA-PRUEBAS/pruebas/src/main/java/com/carlos/pruebas/lexerParser
-cp ParserGraphicsSym.java ../APP-JAVA-PRUEBAS/pruebas/src/main/java/com/carlos/pruebas/lexerParser
+cp Lexer.java tmp/pruebas/src/main/java/com/carlos/pruebas/lexerParser/
+cp ParserGraphics.java  tmp/pruebas/src/main/java/com/carlos/pruebas/lexerParser/
+cp ParserGraphicsSym.java  tmp/pruebas/src/main/java/com/carlos/pruebas/lexerParser/
 
 echo "DELETE TEMP FILES"
 rm Lexer.java 
